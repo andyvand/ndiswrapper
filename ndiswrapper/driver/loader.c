168f12a6769da016f00a90c8834f323e5ca97a7a
pof <pof@388e3070-e2af-4704-926e-a220f121eef5>
2004-03-25 07:42:57 +0000

    Update/Add copyright header in all times

--- ndiswrapper/driver/loader.c
+++ ndiswrapper/driver/loader.c
@@ -1,5 +1,5 @@
 /*
- *  Copyright (C) 2003 Pontus Fuchs
+ *  Copyright (C) 2003-2004 Pontus Fuchs, Giridhar Pemmasani
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -12,7 +12,6 @@
  *  GNU General Public License for more details.
  *
  */
-
 #include <linux/module.h>
 #include <linux/mm.h>
 #include <asm/pgalloc.h>
