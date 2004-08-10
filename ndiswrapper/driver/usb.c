9ac468b054d640a43406841635c2754f1cb2907b
kiszka <kiszka@388e3070-e2af-4704-926e-a220f121eef5>
2004-08-10 20:14:29 +0000

    Moved urb completion handling in tasklet context. Fixed (hopefully) interrupt urb interval value of high speed devices. Both patches triggered by Uli Luckas.

--- ndiswrapper/driver/usb.c
+++ ndiswrapper/driver/usb.c
@@ -16,65 +16,112 @@
 #include "ndis.h"
 #include "usb.h"
 
+
+LIST_HEAD(completed_irps);
+static spinlock_t completed_irps_lock = SPIN_LOCK_UNLOCKED;
+
+
+void usb_transfer_complete_tasklet(unsigned long dummy);
+DECLARE_TASKLET(completed_irps_tasklet, usb_transfer_complete_tasklet, 0);
+
+
 void usb_transfer_complete(struct urb *urb, struct pt_regs *regs)
 {
 	struct irp *irp = urb->context;
-	struct io_stack_location *stack = irp->current_stack_location-1;
-	union nt_urb *nt_urb = stack->params.generic.arg1;
 
 
-	TRACEENTER3("urb = %p, status = %d", urb, urb->status);
-	irp->pending_returned = 1;
+	TRACEENTER3("urb = %p", urb);
 
-	if (urb->status)
-		irp->io_status.status = STATUS_FAILURE;
-	else
-		irp->io_status.status = STATUS_SUCCESS;
-	irp->io_status.status_info = urb->actual_length;
+	spin_lock(&completed_irps_lock);
+	list_add_tail((struct list_head *)&irp->list_entry, &completed_irps);
+	spin_unlock(&completed_irps_lock);
 
-	if (irp->user_status) {
-		irp->user_status->status = irp->io_status.status;
-		irp->user_status->status_info = urb->actual_length;
-	}
+	tasklet_schedule(&completed_irps_tasklet);
+}
 
-	nt_urb->bulkIntrTrans.transferBufLen = urb->actual_length;
+void usb_transfer_complete_tasklet(unsigned long dummy)
+{
+	struct irp *irp;
+	struct urb *urb;
+	struct io_stack_location *stack;
+	union nt_urb *nt_urb;
+	unsigned long flags;
+	unsigned long result;
+
+
+	while (1) {
+		spin_lock_irqsave(&completed_irps_lock, flags);
+
+		if (list_empty(&completed_irps)) {
+			spin_unlock_irqrestore(&completed_irps_lock, flags);
+			TRACEEXIT3(return);
+		}
+		irp = container_of((struct list_entry *)completed_irps.next,
+			struct irp, list_entry);
+		list_del((struct list_head *)&irp->list_entry);
+
+		spin_unlock_irqrestore(&completed_irps_lock, flags);
+
+		urb    = irp->driver_context[3];
+		stack  = irp->current_stack_location-1;
+		nt_urb = stack->params.generic.arg1;
+
+		DBGTRACE3("irp = %p, urb = %p, status = %d", irp, urb,
+			urb->status);
+
+		irp->pending_returned = 1;
+
+		if (urb->status)
+			irp->io_status.status = STATUS_FAILURE;
+		else
+			irp->io_status.status = STATUS_SUCCESS;
+		irp->io_status.status_info = urb->actual_length;
+
+		if (irp->user_status) {
+			irp->user_status->status = irp->io_status.status;
+			irp->user_status->status_info = urb->actual_length;
+		}
+
+		nt_urb->bulkIntrTrans.transferBufLen = urb->actual_length;
 
 #ifdef DUMPURBS
-	if ((urb->pipe & USB_DIR_IN)) {
-		int i;
+		if ((urb->pipe & USB_DIR_IN)) {
+			int i;
 
-		printk("Receiving ");
-		for (i = 0; i < urb->actual_length; i++)
-			printk("%02X ", *(((unsigned char *)urb->transfer_buffer)+i));
-		printk("\n");
-	}
+			printk("Receiving ");
+			for (i = 0; i < urb->actual_length; i++)
+				printk("%02X ", *(((unsigned char *)
+					urb->transfer_buffer)+i));
+			printk("\n");
+		}
 #endif /* DUMPURBS */
 
-	if (stack->completion_handler) {
-		if (((urb->status == 0) &&
-		     (stack->control & CALL_ON_SUCCESS)) ||
-		    ((urb->status != 0) &&
-		     (stack->control & CALL_ON_ERROR))) {
+		if ((stack->completion_handler) &&
+		    ((((urb->status == 0) &&
+		       (stack->control & CALL_ON_SUCCESS)) ||
+		      ((urb->status != 0) &&
+		       (stack->control & CALL_ON_ERROR))))) {
 			DBGTRACE3("calling %p", stack->completion_handler);
-			if (stack->completion_handler(stack->dev_obj, irp,
-			                              stack->handler_arg) ==
-			    STATUS_MORE_PROCESSING_REQUIRED)
-				TRACEEXIT3(return);
+
+			result = stack->completion_handler(stack->dev_obj, irp,
+				stack->handler_arg);
+			if (result == STATUS_MORE_PROCESSING_REQUIRED) {
+				usb_free_urb(urb);
+				continue;
+			}
 		}
+
+		if (irp->user_event) {
+			DBGTRACE3("setting event %p", irp->user_event);
+			NdisSetEvent((struct ndis_event *)irp->user_event);
+		}
+
+		/* To-Do: what about IRP_DEALLOCATE_BUFFER...? */
+		DBGTRACE("freeing irp %p", irp);
+		kfree(irp);
+
+		usb_free_urb(urb);
 	}
-
-	if (irp->user_event) {
-		DBGTRACE3("setting event %p", irp->user_event);
-		NdisSetEvent((struct ndis_event *)irp->user_event);
-	}
-
-	/* To-Do: what about IRP_DEALLOCATE_BUFFER...? */
-	DBGTRACE("freeing irp %p", irp);
-	kfree(irp);
-
-	usb_free_urb(urb);
-
-	TRACEEXIT3(return);
 }
 
 void STDCALL usb_cancel_transfer(struct device_object *dev_obj,
@@ -179,7 +226,7 @@ unsigned long usb_bulk_or_intr_trans(struct usb_device *dev,
 	/* XXX we should better check what GFP_ is required XXX */
 	ret = usb_submit_urb(urb, GFP_ATOMIC);
 	if (ret != 0) {
-		DBGTRACE3("usb_submit_urb() = %d", ret);
+		ERROR("usb_submit_urb() = %d", ret);
 		usb_free_urb(urb);
 		if (irp->driver_context[2])
 			kfree(irp->driver_context[2]);
@@ -288,6 +335,8 @@ unsigned long usb_submit_nt_urb(struct usb_device *dev, union nt_urb *nt_urb,
 					endp->desc.bmAttributes &
 					USB_ENDPOINT_XFERTYPE_MASK;
 				pipe_info->pipeHandle.encoded.interval =
+					(dev->speed == USB_SPEED_HIGH) ?
+					endp->desc.bInterval + 3 :
 					endp->desc.bInterval;
 				pipe_info->pipeHandle.encoded.fill = 0;
 
