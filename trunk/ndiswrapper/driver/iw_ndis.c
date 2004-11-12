89ada6003f6dbeb024e0612ac6a104e6637ca2c0
pgiri <pgiri@388e3070-e2af-4704-926e-a220f121eef5>
2004-11-12 23:20:53 +0000

    With the latest driver for DWL-520+, ACX 100 chipset works with WPA+TKIP and doesn't need extra checks in ndis_translate_scan for getting WPA IE.

--- trunk/ndiswrapper/driver/iw_ndis.c
+++ trunk/ndiswrapper/driver/iw_ndis.c
@@ -863,9 +863,9 @@ static char *ndis_translate_scan(struct net_device *dev, char *event,
 	iwe.u.data.length = strlen(buf);
 	event = iwe_stream_add_point(event, end_buf, &iwe, buf);
 	
-	if (item->length > ((char *)&item->rates[NDIS_MAX_RATES] -
-			    (char *)&item->length) &&
-	    item->ie_length >= (sizeof(struct fixed_ies) + 2))
+//	if (item->length > ((char *)&item->rates[NDIS_MAX_RATES] -
+//			    (char *)&item->length) &&
+	if (item->ie_length >= (sizeof(struct fixed_ies) + 2))
 	{
 		struct fixed_ies *fixed_ies = (struct fixed_ies *)item->ies;
 		unsigned char *iep = (unsigned char *)(fixed_ies + 1);
