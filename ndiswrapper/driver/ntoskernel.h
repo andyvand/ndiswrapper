63833c1a223d21665e4b10c6aff909815ff781f8
pgiri <pgiri@388e3070-e2af-4704-926e-a220f121eef5>
2005-10-25 10:39:19 +0000

    If urb couldn't be allocated, return NULL in wrap_alloc_urb; else chaos reigns.

--- ndiswrapper/driver/ntoskernel.h
+++ ndiswrapper/driver/ntoskernel.h
@@ -793,11 +793,11 @@ while (1) {								\
 do {									\
 	KIRQL _cur_irql_ = current_irql();				\
 	CHECK_KSPIN_LOCKED(lock);					\
+	kspin_unlock(lock);						\
 	if (oldirql < DISPATCH_LEVEL && _cur_irql_ == DISPATCH_LEVEL) {	\
 		preempt_enable_no_resched();				\
 		local_bh_enable();					\
 	}								\
-	kspin_unlock(lock);						\
 } while (0)
 
 #define kspin_lock_irqsave(lock, flags)					\
@@ -809,9 +809,9 @@ do {									\
 
 #define kspin_unlock_irqrestore(lock, flags)				\
 do {									\
-	local_irq_restore(flags);					\
-	preempt_enable_no_resched();					\
 	kspin_unlock(lock);						\
+	local_irq_restore(flags);					\
+	preempt_enable();						\
 } while (0)
 
 static inline ULONG SPAN_PAGES(ULONG_PTR ptr, SIZE_T length)
