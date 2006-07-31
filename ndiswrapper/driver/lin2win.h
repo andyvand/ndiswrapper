ac73b02e27eb84838c57abf47704e86ed856c7ec
pgiri <pgiri@388e3070-e2af-4704-926e-a220f121eef5>
2006-07-31 20:42:43 +0000

    Although Windows function doesn't use r10, r11 for parameters, these are volatile (i.e., Windows function can use them without saving). So put them in clobber list. This also means that we don't have to save r10 and r11 in win2lin functions. Will try that next and wait for people to test.

--- ndiswrapper/driver/lin2win.h
+++ ndiswrapper/driver/lin2win.h
@@ -15,14 +15,14 @@
 
 #ifdef CONFIG_X86_64
 
-#if 0
+#if 1
 
 /* Windows functions must have 32 bytes of reserved space above return
  * address, irrespective of number of args. So argc >= 4 */
 
-#define alloc_win_stack_frame(argc)					\
+#define alloc_win_stack_frame(argc)		\
 	"subq $" #argc "*8, %%rsp\n\t"
-#define free_win_stack_frame(argc)					\
+#define free_win_stack_frame(argc)		\
 	"addq $" #argc "*8, %%rsp\n\t"
 
 /* m is index of Windows arg required, n is total number of args to
@@ -45,7 +45,7 @@
 		free_win_stack_frame(4)					\
 		: "=a" (ret), "=c" (dummy), "=d" (dummy)		\
 		: "a" (func)						\
-		: "r8", "r9");						\
+		: "r8", "r9", "r10", "r11");				\
 	DBGTRACE6("%p done", func);					\
 	ret;								\
 })
@@ -61,7 +61,7 @@
 		: "=a" (ret), "=c" (dummy), "=d" (dummy)		\
 		: "c" (arg1),						\
 		  "a" (func)						\
-		: "r8", "r9");						\
+		: "r8", "r9", "r10", "r11");				\
 	DBGTRACE6("%p done", func);					\
 	ret;								\
 })
@@ -77,7 +77,7 @@
 		: "=a" (ret), "=c" (dummy), "=d" (dummy)		\
 		: "c" (arg1), "d" (arg2),				\
 		  "a" (func)						\
-		: "r8", "r9");						\
+		: "r8", "r9", "r10", "r11");				\
 	DBGTRACE6("%p done", func);					\
 	ret;								\
 })
@@ -94,7 +94,7 @@
 		: "=a" (ret), "=c" (dummy), "=d" (dummy)		\
 		: "c" (arg1), "d" (arg2), "g" ((u64)arg3),		\
 		  "a" (func)						\
-		: "r8", "r9");						\
+		: "r8", "r9", "r10", "r11");				\
 	DBGTRACE6("%p done", func);					\
 	ret;								\
 })
@@ -113,7 +113,7 @@
 		: "c" (arg1), "d" (arg2), "g" ((u64)arg3),		\
 		  "g" ((u64)arg4),					\
 		  "a" (func)						\
-		: "r8", "r9");						\
+		: "r8", "r9", "r10", "r11");				\
 	DBGTRACE6("%p done", func);					\
 	ret;								\
 })
@@ -125,15 +125,16 @@
 	__asm__ __volatile__(						\
 		"movq %5, %%r8\n\t"					\
 		"movq %6, %%r9\n\t"					\
-		"movq %7, " lin2win_win_arg(5,5) "\n\t"			\
+		"movq %7, %%r10\n\t"					\
+		"movq %%r10, " lin2win_win_arg(5,5) "\n\t"		\
 		alloc_win_stack_frame(5)				\
 		"call *%8\n\t"						\
 		free_win_stack_frame(5)					\
 		: "=a" (ret), "=c" (dummy), "=d" (dummy)		\
 		: "c" (arg1), "d" (arg2), "g" ((u64)arg3),		\
-		  "g" ((u64)arg4), "ri" ((u64)arg5),			\
+		  "g" ((u64)arg4), "g" ((u64)arg5),			\
 		  "a" (func)						\
-		: "r8", "r9");						\
+		: "r8", "r9", "r10", "r11");				\
 	DBGTRACE6("%p done", func);					\
 	ret;								\
 })
@@ -145,16 +146,18 @@
 	__asm__ __volatile__(						\
 		"movq %5, %%r8\n\t"					\
 		"movq %6, %%r9\n\t"					\
-		"movq %7, " lin2win_win_arg(5,6) "\n\t"			\
-		"movq %8, " lin2win_win_arg(6,6) "\n\t"			\
+		"movq %7, %%r10\n\t"					\
+		"movq %%r10, " lin2win_win_arg(5,6) "\n\t"		\
+		"movq %8, %%r10\n\t"					\
+		"movq %%r10, " lin2win_win_arg(6,6) "\n\t"		\
 		alloc_win_stack_frame(6)				\
 		"call *%9\n\t"						\
 		free_win_stack_frame(6)					\
 		: "=a" (ret), "=c" (dummy), "=d" (dummy)		\
 		: "c" (arg1), "d" (arg2), "g" ((u64)arg3),		\
-		  "g" ((u64)arg4), "ri" ((u64)arg5), "ri" ((u64)arg6),	\
+		  "g" ((u64)arg4), "g" ((u64)arg5), "g" ((u64)arg6),	\
 		  "a" (func)						\
-		: "r8", "r9");						\
+		: "r8", "r9", "r10", "r11");				\
 	DBGTRACE6("%p done", func);					\
 	ret;								\
 })
