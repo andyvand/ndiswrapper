1777cd8d3151cfb3c3447bd0128416a70656321f
pgiri <pgiri@388e3070-e2af-4704-926e-a220f121eef5>
2006-11-30 14:13:04 +0000

    Signal handling is different in 2.4 and 2.6 kernels - this version should work for latest 2.6 kernel (need to look into 2.4 kernels and make it compatible for both 2.4 and 2.6).

--- ndiswrapper/driver/ntoskernel.c
+++ ndiswrapper/driver/ntoskernel.c
@@ -1768,6 +1768,7 @@ static int thread_trampoline(void *data)
 {
 	struct trampoline_context ctx;
 	struct nt_thread *thread;
+	sigset_t blocked;
 
 	memcpy(&ctx, data, sizeof(ctx));
 	kfree(data);
@@ -1781,15 +1782,9 @@ static int thread_trampoline(void *data)
 	current->comm[sizeof(current->comm)-1] = 0;
 	DBGTRACE2("thread: %p, task: %p (%d)", thread, thread->task,
 		  thread->pid);
-	spin_lock_irq(SIG_LOCK(current));
-	sigemptyset(&current->blocked);
+	sigfillset(&blocked);
+	sigprocmask(SIG_BLOCK, &blocked, NULL);
 	flush_signals(current);
-#if LINUX_VERSION_CODE < KERNEL_VERSION(2,6,7)
-	recalc_sigpending(current);
-#else
-	recalc_sigpending();
-#endif
-	spin_unlock_irq(SIG_LOCK(current));
 	LIN2WIN1(ctx.start_routine, ctx.context);
 	DBGTRACE2("thread: %p, task: %p (%d)", thread, thread->task,
 		  thread->pid);
