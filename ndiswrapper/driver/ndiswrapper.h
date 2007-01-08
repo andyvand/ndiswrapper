4cfac50ff1df813400960f351cb0005a73cc997d
pgiri <pgiri@388e3070-e2af-4704-926e-a220f121eef5>
2007-01-08 17:10:09 +0000

    Prepare 1.34

--- ndiswrapper/driver/ndiswrapper.h
+++ ndiswrapper/driver/ndiswrapper.h
@@ -16,7 +16,7 @@
 #ifndef _NDISWRAPPER_H_
 #define _NDISWRAPPER_H_
 
-#define DRIVER_VERSION "1.34rc1"
+#define DRIVER_VERSION "1.34"
 #define UTILS_VERSION "1.9"
 
 #define DRIVER_NAME "ndiswrapper"
