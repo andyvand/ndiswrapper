af68d086dfe3098330a2c6388319841a3b242607
pgiri <pgiri@388e3070-e2af-4704-926e-a220f121eef5>
2007-09-06 15:54:17 +0000

    Prepare 1.48rc2

--- trunk/ndiswrapper/driver/ndiswrapper.h
+++ trunk/ndiswrapper/driver/ndiswrapper.h
@@ -16,7 +16,7 @@
 #ifndef _NDISWRAPPER_H_
 #define _NDISWRAPPER_H_
 
-#define DRIVER_VERSION "1.48rc1"
+#define DRIVER_VERSION "1.48rc2"
 #define UTILS_VERSION "1.9"
 
 #define DRIVER_NAME "ndiswrapper"
