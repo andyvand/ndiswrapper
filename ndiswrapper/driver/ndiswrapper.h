302a91ba8e6a01ed86c124d97751a6e5c74bfa95
pgiri <pgiri@388e3070-e2af-4704-926e-a220f121eef5>
2007-09-28 15:51:37 +0000

    Prepare 1.49rc2

--- ndiswrapper/driver/ndiswrapper.h
+++ ndiswrapper/driver/ndiswrapper.h
@@ -16,7 +16,7 @@
 #ifndef _NDISWRAPPER_H_
 #define _NDISWRAPPER_H_
 
-#define DRIVER_VERSION "1.49rc1"
+#define DRIVER_VERSION "1.49rc2"
 #define UTILS_VERSION "1.9"
 
 #define DRIVER_NAME "ndiswrapper"
