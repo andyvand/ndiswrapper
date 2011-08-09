b6a87c6284c892a9f0d1555874f0fd33244a65dd
proski <proski@388e3070-e2af-4704-926e-a220f121eef5>
2011-08-09 15:24:35 +0000

    Use explicit casts between enums

--- trunk/ndiswrapper/driver/ntoskernel.c
+++ trunk/ndiswrapper/driver/ntoskernel.c
@@ -462,7 +462,7 @@ void wrap_init_timer(struct nt_timer *nt_timer, enum timer_type type,
 #endif
 	nt_timer->wrap_timer = wrap_timer;
 	nt_timer->kdpc = NULL;
-	initialize_object(&nt_timer->dh, type, 0);
+	initialize_object(&nt_timer->dh, (enum dh_type)type, 0);
 	nt_timer->wrap_timer_magic = WRAP_TIMER_MAGIC;
 	TIMERTRACE("timer %p (%p)", wrap_timer, nt_timer);
 	spin_lock_bh(&ntoskernel_lock);
@@ -1204,7 +1204,7 @@ wstdcall void WIN_FUNC(KeInitializeEvent,3)
 	(struct nt_event *nt_event, enum event_type type, BOOLEAN state)
 {
 	EVENTENTER("event = %p, type = %d, state = %d", nt_event, type, state);
-	initialize_object(&nt_event->dh, type, state);
+	initialize_object(&nt_event->dh, (enum dh_type)type, state);
 	EVENTEXIT(return);
 }
 
