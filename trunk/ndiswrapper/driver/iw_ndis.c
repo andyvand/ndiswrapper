6873936204d1cd4179689df63237d2aa8acf7a50
proski <proski@388e3070-e2af-4704-926e-a220f121eef5>
2011-08-16 17:15:12 +0000

    Remove no_warn_unused, it's never needed

--- trunk/ndiswrapper/driver/iw_ndis.c
+++ trunk/ndiswrapper/driver/iw_ndis.c
@@ -1149,7 +1149,7 @@ static char *ndis_translate_scan(struct net_device *dev,
 	if (bssid->length > sizeof(*bssid)) {
 		unsigned char *iep = (unsigned char *)bssid_ex->ies +
 			sizeof(struct ndis_fixed_ies);
-		no_warn_unused unsigned char *end = iep + bssid_ex->ie_length;
+		unsigned char *end = iep + bssid_ex->ie_length;
 
 		while (iep + 1 < end && iep + 2 + iep[1] <= end) {
 			unsigned char ielen = 2 + iep[1];
