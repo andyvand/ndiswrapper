542a1e39c938c2c4fc360b564b2307013d8dfa18
proski <proski@388e3070-e2af-4704-926e-a220f121eef5>
2011-08-22 16:44:11 +0000

    Resolve minor differences with the version in ndis6wrapper

--- ndiswrapper/driver/rtl.c
+++ ndiswrapper/driver/rtl.c
@@ -1,5 +1,6 @@
 /*
  *  Copyright (C) 2003-2005 Pontus Fuchs, Giridhar Pemmasani
+ *  Copyright (C) 2006-2007 Giridhar Pemmasani
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -22,6 +23,7 @@ wstdcall SIZE_T WIN_FUNC(RtlCompareMemory,3)
 	size_t i;
 	char *x, *y;
 
+	ENTER1("%p %p %zd", a, b, len);
 	x = (char *)a;
 	y = (char *)b;
 	/* MSDN says this should return number of bytes that compare as
