87ecd82d452b7cc47a12ba33fac619b4aae26343
proski <proski@388e3070-e2af-4704-926e-a220f121eef5>
2011-08-23 06:03:57 +0000

    Warn about NULL passed to IoFreeIrp

--- trunk/ndiswrapper/driver/ntoskernel_io.c
+++ trunk/ndiswrapper/driver/ntoskernel_io.c
@@ -164,8 +164,10 @@ wstdcall void WIN_FUNC(IoFreeIrp,1)
 	(struct irp *irp)
 {
 	IOENTER("irp = %p", irp);
-	if (!irp)
+	if (!irp) {
+		WARNING("irp is NULL");
 		return;
+	}
 
 	if (irp->flags & IRP_SYNCHRONOUS_API)
 		IoDequeueThreadIrp(irp);
