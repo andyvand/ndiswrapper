c6dca923b353441636d6c940b315c77af6406a37
proski <proski@388e3070-e2af-4704-926e-a220f121eef5>
2011-09-08 21:44:16 +0000

    Rewrite lin2win.S based on win2lin_stubs.S
    The hackish universal lin2win() was confusing the Linux stack unwind
    algorithm.  Replace it with individual functions for a different number
    of arguments.  Create proper stack frame.  Provide CFI information.

--- ndiswrapper/driver/lin2win.h
+++ ndiswrapper/driver/lin2win.h
@@ -15,57 +15,64 @@
 
 #ifdef CONFIG_X86_64
 
-u64 lin2win(void *func, ...);
+u64 lin2win0(void *func);
+u64 lin2win1(void *func, u64 arg1);
+u64 lin2win2(void *func, u64 arg1, u64 arg2);
+u64 lin2win3(void *func, u64 arg1, u64 arg2, u64 arg3);
+u64 lin2win4(void *func, u64 arg1, u64 arg2, u64 arg3, u64 arg4);
+u64 lin2win5(void *func, u64 arg1, u64 arg2, u64 arg3, u64 arg4, u64 arg5);
+u64 lin2win6(void *func, u64 arg1, u64 arg2, u64 arg3, u64 arg4, u64 arg5,
+	     u64 arg6);
 
 #define LIN2WIN0(func)							\
 ({									\
 	if (0)								\
 		func();							\
-	lin2win(func);						\
+	lin2win0(func);							\
 })
 
 #define LIN2WIN1(func, arg1)						\
 ({									\
 	if (0)								\
 		func(arg1);						\
-	lin2win(func, 0, (u64)arg1);					\
+	lin2win1(func, (u64)arg1);					\
 })
 
 #define LIN2WIN2(func, arg1, arg2)					\
 ({									\
 	if (0)								\
 		func(arg1, arg2);					\
-	lin2win(func, 0, (u64)arg1, (u64)arg2);			\
+	lin2win2(func, (u64)arg1, (u64)arg2);			\
 })
 
 #define LIN2WIN3(func, arg1, arg2, arg3)				\
 ({									\
 	if (0)								\
 		func(arg1, arg2, arg3);					\
-	lin2win(func, 0, (u64)arg1, (u64)arg2, (u64)arg3);		\
+	lin2win3(func, (u64)arg1, (u64)arg2, (u64)arg3);		\
 })
 
 #define LIN2WIN4(func, arg1, arg2, arg3, arg4)				\
 ({									\
 	if (0)								\
 		func(arg1, arg2, arg3, arg4);				\
-	lin2win(func, 0, (u64)arg1, (u64)arg2, (u64)arg3, (u64)arg4);	\
+	lin2win4(func, (u64)arg1, (u64)arg2, (u64)arg3, (u64)arg4);	\
 })
 
 #define LIN2WIN5(func, arg1, arg2, arg3, arg4, arg5)			\
 ({									\
 	if (0)								\
 		func(arg1, arg2, arg3, arg4, arg5);			\
-	lin2win(func, 0, (u64)arg1, (u64)arg2, (u64)arg3, (u64)arg4,	\
-		  (u64)arg5);						\
+	lin2win5(func, (u64)arg1, (u64)arg2, (u64)arg3, (u64)arg4,	\
+		 (u64)arg5);						\
 })
 
 #define LIN2WIN6(func, arg1, arg2, arg3, arg4, arg5, arg6)		\
 ({									\
 	if (0)								\
 		func(arg1, arg2, arg3, arg4, arg5, arg6);		\
-	lin2win(func, 0, (u64)arg1, (u64)arg2, (u64)arg3, (u64)arg4,	\
-		  (u64)arg5, (u64)arg6);				\
+	lin2win6(func, (u64)arg1, (u64)arg2, (u64)arg3, (u64)arg4,	\
+		 (u64)arg5, (u64)arg6);					\
 })
 
 #else // CONFIG_X86_64
