31345265aca3a60c08d5b7efa09f35ae06f94174
proski <proski@388e3070-e2af-4704-926e-a220f121eef5>
2011-09-08 15:36:58 +0000

    Rewrite 64-bit lin2win in pure assembler, ensure 16-bit stack alignment
    lin2win.S is a clever piece of code that reused the existing stack frame
    instead of allocating a new one.

--- branches/ndisv6/ndiswrapper/driver/lin2win.h
+++ branches/ndisv6/ndiswrapper/driver/lin2win.h
@@ -15,169 +15,57 @@
 
 #ifdef CONFIG_X86_64
 
-/* Windows functions must have 32 bytes of shadow space for arguments
- * above return address, irrespective of number of args. So argc >= 4
- */
-
-#define alloc_win_stack_frame(argc)		\
-	"sub $(" #argc ")*8, %%rsp\n\t"
-#define free_win_stack_frame(argc)		\
-	"add $(" #argc ")*8, %%rsp\n\t"
-
-/* m is index of Windows arg required; Windows arg 1 should be at
- * 0(%rsp), arg 2 at 8(%rsp) and so on after the frame is allocated.
-*/
-
-#define lin2win_win_arg(m) "(" #m "-1)*8(%%rsp)"
-
-/* args for Windows function must be in clobber / output list */
-
-#define outputs()							\
-	"=a" (_ret), "=c" (_dummy), "=d" (_dummy),			\
-		"=r" (r8), "=r" (r9), "=r" (r10), "=r" (r11)
-
-#define clobbers() "cc", "memory"
+u64 lin2win(void *func, ...);
 
 #define LIN2WIN0(func)							\
 ({									\
-	u64 _ret, _dummy;						\
-	register u64 r8 __asm__("r8");					\
-	register u64 r9 __asm__("r9");					\
-	register u64 r10 __asm__("r10");				\
-	register u64 r11 __asm__("r11");				\
-	__asm__ __volatile__(						\
-		alloc_win_stack_frame(4)				\
-		"callq *%[fptr]\n\t"					\
-		free_win_stack_frame(4)					\
-		: outputs()						\
-		: [fptr] "r" (func)					\
-		: clobbers());						\
 	if (0)								\
 		func();							\
-	_ret;								\
+	lin2win(func);						\
 })
 
 #define LIN2WIN1(func, arg1)						\
 ({									\
-	u64 _ret, _dummy;						\
-	register u64 r8 __asm__("r8");					\
-	register u64 r9 __asm__("r9");					\
-	register u64 r10 __asm__("r10");				\
-	register u64 r11 __asm__("r11");				\
-	__asm__ __volatile__(						\
-		alloc_win_stack_frame(4)				\
-		"callq *%[fptr]\n\t"					\
-		free_win_stack_frame(4)					\
-		: outputs()						\
-		: "c" (arg1), [fptr] "r" (func)				\
-		: clobbers());						\
 	if (0)								\
 		func(arg1);						\
-	_ret;								\
+	lin2win(func, 0, (u64)arg1);					\
 })
 
 #define LIN2WIN2(func, arg1, arg2)					\
 ({									\
-	u64 _ret, _dummy;						\
-	register u64 r8 __asm__("r8");					\
-	register u64 r9 __asm__("r9");					\
-	register u64 r10 __asm__("r10");				\
-	register u64 r11 __asm__("r11");				\
-	__asm__ __volatile__(						\
-		alloc_win_stack_frame(4)				\
-		"callq *%[fptr]\n\t"					\
-		free_win_stack_frame(4)					\
-		: outputs()						\
-		: "c" (arg1), "d" (arg2), [fptr] "r" (func)		\
-		: clobbers());						\
 	if (0)								\
 		func(arg1, arg2);					\
-	_ret;								\
+	lin2win(func, 0, (u64)arg1, (u64)arg2);			\
 })
 
 #define LIN2WIN3(func, arg1, arg2, arg3)				\
 ({									\
-	u64 _ret, _dummy;						\
-	register u64 r8 __asm__("r8") = (u64)arg3;			\
-	register u64 r9 __asm__("r9");					\
-	register u64 r10 __asm__("r10");				\
-	register u64 r11 __asm__("r11");				\
-	__asm__ __volatile__(						\
-		alloc_win_stack_frame(4)				\
-		"callq *%[fptr]\n\t"					\
-		free_win_stack_frame(4)					\
-		: outputs()						\
-		: "c" (arg1), "d" (arg2), "r" (r8),			\
-		  [fptr] "r" (func)					\
-		: clobbers());						\
 	if (0)								\
 		func(arg1, arg2, arg3);					\
-	_ret;								\
+	lin2win(func, 0, (u64)arg1, (u64)arg2, (u64)arg3);		\
 })
 
 #define LIN2WIN4(func, arg1, arg2, arg3, arg4)				\
 ({									\
-	u64 _ret, _dummy;						\
-	register u64 r8 __asm__("r8") = (u64)arg3;			\
-	register u64 r9 __asm__("r9") = (u64)arg4;			\
-	register u64 r10 __asm__("r10");				\
-	register u64 r11 __asm__("r11");				\
-	__asm__ __volatile__(						\
-		alloc_win_stack_frame(4)				\
-		"callq *%[fptr]\n\t"					\
-		free_win_stack_frame(4)					\
-		: outputs()						\
-		: "c" (arg1), "d" (arg2), "r" (r8), "r" (r9),		\
-		  [fptr] "r" (func)					\
-		: clobbers());						\
 	if (0)								\
 		func(arg1, arg2, arg3, arg4);				\
-	_ret;								\
+	lin2win(func, 0, (u64)arg1, (u64)arg2, (u64)arg3, (u64)arg4);	\
 })
 
 #define LIN2WIN5(func, arg1, arg2, arg3, arg4, arg5)			\
 ({									\
-	u64 _ret, _dummy;						\
-	register u64 r8 __asm__("r8") = (u64)arg3;			\
-	register u64 r9 __asm__("r9") = (u64)arg4;			\
-	register u64 r10 __asm__("r10");				\
-	register u64 r11 __asm__("r11");				\
-	__asm__ __volatile__(						\
-		alloc_win_stack_frame(5)				\
-		"movq %[rarg5], " lin2win_win_arg(5) "\n\t"		\
-		"callq *%[fptr]\n\t"					\
-		free_win_stack_frame(5)					\
-		: outputs()						\
-		: "c" (arg1), "d" (arg2), "r" (r8), "r" (r9),		\
-		  [rarg5] "ri" ((u64)arg5),				\
-		  [fptr] "r" (func)					\
-		: clobbers());						\
 	if (0)								\
 		func(arg1, arg2, arg3, arg4, arg5);			\
-	_ret;								\
+	lin2win(func, 0, (u64)arg1, (u64)arg2, (u64)arg3, (u64)arg4,	\
+		  (u64)arg5);						\
 })
 
 #define LIN2WIN6(func, arg1, arg2, arg3, arg4, arg5, arg6)		\
 ({									\
-	u64 _ret, _dummy;						\
-	register u64 r8 __asm__("r8") = (u64)arg3;			\
-	register u64 r9 __asm__("r9") = (u64)arg4;			\
-	register u64 r10 __asm__("r10");				\
-	register u64 r11 __asm__("r11");				\
-	__asm__ __volatile__(						\
-		alloc_win_stack_frame(6)				\
-		"movq %[rarg5], " lin2win_win_arg(5) "\n\t"		\
-		"movq %[rarg6], " lin2win_win_arg(6) "\n\t"		\
-		"callq *%[fptr]\n\t"					\
-		free_win_stack_frame(6)					\
-		: outputs()						\
-		: "c" (arg1), "d" (arg2), "r" (r8), "r" (r9),		\
-		  [rarg5] "ri" ((u64)arg5), [rarg6] "ri" ((u64)arg6),	\
-		  [fptr] "r" (func)					\
-		: clobbers());						\
 	if (0)								\
 		func(arg1, arg2, arg3, arg4, arg5, arg6);		\
-	_ret;								\
+	lin2win(func, 0, (u64)arg1, (u64)arg2, (u64)arg3, (u64)arg4,	\
+		  (u64)arg5, (u64)arg6);				\
 })
 
 #else // CONFIG_X86_64
