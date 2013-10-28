acbd5154ff49cec0a9659558e95c66e7a0d88527
proski <proski@388e3070-e2af-4704-926e-a220f121eef5>
2013-10-28 19:16:07 +0000

    Prevent buffer overflow in add_text() rather than warn after the fact
    Replace pointer p with integer len to avoid pointer subtraction and
    improve code readability.  Don't pass p to add_text().

--- ndiswrapper/driver/proc.c
+++ ndiswrapper/driver/proc.c
@@ -46,14 +46,15 @@ void proc_remove(struct proc_dir_entry *de)
 }
 #endif
 
-#define add_text(p, fmt, ...) (p += sprintf(p, fmt, ##__VA_ARGS__))
+#define add_text(fmt, ...) \
+	(len += scnprintf(page + len, count - len, fmt, ##__VA_ARGS__))
 
 static struct proc_dir_entry *wrap_procfs_entry;
 
 static int procfs_read_ndis_stats(char *page, char **start, off_t off,
 				  int count, int *eof, void *data)
 {
-	char *p = page;
+	int len = 0;
 	struct ndis_device *wnd = (struct ndis_device *)data;
 	struct ndis_wireless_stats stats;
 	NDIS_STATUS res;
@@ -66,37 +67,31 @@ static int procfs_read_ndis_stats(char *page, char **start, off_t off,
 
 	res = mp_query(wnd, OID_802_11_RSSI, &rssi, sizeof(rssi));
 	if (!res)
-		add_text(p, "signal_level=%d dBm\n", (s32)rssi);
+		add_text("signal_level=%d dBm\n", (s32)rssi);
 
 	res = mp_query(wnd, OID_802_11_STATISTICS, &stats, sizeof(stats));
 	if (!res) {
-		add_text(p, "tx_frames=%llu\n", stats.tx_frag);
-		add_text(p, "tx_multicast_frames=%llu\n", stats.tx_multi_frag);
-		add_text(p, "tx_failed=%llu\n", stats.failed);
-		add_text(p, "tx_retry=%llu\n", stats.retry);
-		add_text(p, "tx_multi_retry=%llu\n", stats.multi_retry);
-		add_text(p, "tx_rtss_success=%llu\n", stats.rtss_succ);
-		add_text(p, "tx_rtss_fail=%llu\n", stats.rtss_fail);
-		add_text(p, "ack_fail=%llu\n", stats.ack_fail);
-		add_text(p, "frame_duplicates=%llu\n", stats.frame_dup);
-		add_text(p, "rx_frames=%llu\n", stats.rx_frag);
-		add_text(p, "rx_multicast_frames=%llu\n", stats.rx_multi_frag);
-		add_text(p, "fcs_errors=%llu\n", stats.fcs_err);
+		add_text("tx_frames=%llu\n", stats.tx_frag);
+		add_text("tx_multicast_frames=%llu\n", stats.tx_multi_frag);
+		add_text("tx_failed=%llu\n", stats.failed);
+		add_text("tx_retry=%llu\n", stats.retry);
+		add_text("tx_multi_retry=%llu\n", stats.multi_retry);
+		add_text("tx_rtss_success=%llu\n", stats.rtss_succ);
+		add_text("tx_rtss_fail=%llu\n", stats.rtss_fail);
+		add_text("ack_fail=%llu\n", stats.ack_fail);
+		add_text("frame_duplicates=%llu\n", stats.frame_dup);
+		add_text("rx_frames=%llu\n", stats.rx_frag);
+		add_text("rx_multicast_frames=%llu\n", stats.rx_multi_frag);
+		add_text("fcs_errors=%llu\n", stats.fcs_err);
 	}
 
-	if (p - page > count) {
-		ERROR("wrote %td bytes (limit is %u)\n",
-		      p - page, count);
-		*eof = 1;
-	}
-
-	return p - page;
+	return len;
 }
 
 static int procfs_read_ndis_encr(char *page, char **start, off_t off,
 				 int count, int *eof, void *data)
 {
-	char *p = page;
+	int len = 0;
 	struct ndis_device *wnd = (struct ndis_device *)data;
 	int i, encr_status, auth_mode, infra_mode;
 	NDIS_STATUS res;
@@ -112,48 +107,43 @@ static int procfs_read_ndis_encr(char *page, char **start, off_t off,
 		       &ap_address, sizeof(ap_address));
 	if (res)
 		memset(ap_address, 0, ETH_ALEN);
-	add_text(p, "ap_address=%2.2X", ap_address[0]);
+	add_text("ap_address=%2.2X", ap_address[0]);
 	for (i = 1; i < ETH_ALEN; i++)
-		add_text(p, ":%2.2X", ap_address[i]);
-	add_text(p, "\n");
+		add_text(":%2.2X", ap_address[i]);
+	add_text("\n");
 
 	res = mp_query(wnd, OID_802_11_SSID, &essid, sizeof(essid));
 	if (!res)
-		add_text(p, "essid=%.*s\n", essid.length, essid.essid);
+		add_text("essid=%.*s\n", essid.length, essid.essid);
 
 	res = mp_query_int(wnd, OID_802_11_ENCRYPTION_STATUS, &encr_status);
 	if (!res) {
 		typeof(&wnd->encr_info.keys[0]) tx_key;
-		add_text(p, "tx_key=%u\n", wnd->encr_info.tx_key_index);
-		add_text(p, "key=");
+		add_text("tx_key=%u\n", wnd->encr_info.tx_key_index);
+		add_text("key=");
 		tx_key = &wnd->encr_info.keys[wnd->encr_info.tx_key_index];
 		if (tx_key->length > 0)
 			for (i = 0; i < tx_key->length; i++)
-				add_text(p, "%2.2X", tx_key->key[i]);
+				add_text("%2.2X", tx_key->key[i]);
 		else
-			add_text(p, "off");
-		add_text(p, "\n");
-		add_text(p, "encr_mode=%d\n", encr_status);
+			add_text("off");
+		add_text("\n");
+		add_text("encr_mode=%d\n", encr_status);
 	}
 	res = mp_query_int(wnd, OID_802_11_AUTHENTICATION_MODE, &auth_mode);
 	if (!res)
-		add_text(p, "auth_mode=%d\n", auth_mode);
+		add_text("auth_mode=%d\n", auth_mode);
 	res = mp_query_int(wnd, OID_802_11_INFRASTRUCTURE_MODE, &infra_mode);
-	add_text(p, "mode=%s\n", (infra_mode == Ndis802_11IBSS) ? "adhoc" :
+	add_text("mode=%s\n", (infra_mode == Ndis802_11IBSS) ? "adhoc" :
 		 (infra_mode == Ndis802_11Infrastructure) ? "managed" : "auto");
-	if (p - page > count) {
-		WARNING("wrote %td bytes (limit is %u)",
-			p - page, count);
-		*eof = 1;
-	}
 
-	return p - page;
+	return len;
 }
 
 static int procfs_read_ndis_hw(char *page, char **start, off_t off,
 			       int count, int *eof, void *data)
 {
-	char *p = page;
+	int len = 0;
 	struct ndis_device *wnd = (struct ndis_device *)data;
 	struct ndis_configuration config;
 	enum ndis_power power_mode;
@@ -176,44 +166,43 @@ static int procfs_read_ndis_hw(char *page, char **start, off_t off,
 
 	res = mp_query_int(wnd, OID_GEN_HARDWARE_STATUS, &n);
 	if (res == NDIS_STATUS_SUCCESS && n >= 0 && n < ARRAY_SIZE(hw_status))
-		add_text(p, "status=%s\n", hw_status[n]);
+		add_text("status=%s\n", hw_status[n]);
 
 	res = mp_query(wnd, OID_802_3_CURRENT_ADDRESS, mac, sizeof(mac));
 	if (!res)
-		add_text(p, "mac: " MACSTRSEP "\n", MAC2STR(mac));
+		add_text("mac: " MACSTRSEP "\n", MAC2STR(mac));
 	res = mp_query(wnd, OID_802_11_CONFIGURATION, &config, sizeof(config));
 	if (!res) {
-		add_text(p, "beacon_period=%u msec\n", config.beacon_period);
-		add_text(p, "atim_window=%u msec\n", config.atim_window);
-		add_text(p, "frequency=%u kHz\n", config.ds_config);
-		add_text(p, "hop_pattern=%u\n", config.fh_config.hop_pattern);
-		add_text(p, "hop_set=%u\n", config.fh_config.hop_set);
-		add_text(p, "dwell_time=%u msec\n",
-			 config.fh_config.dwell_time);
+		add_text("beacon_period=%u msec\n", config.beacon_period);
+		add_text("atim_window=%u msec\n", config.atim_window);
+		add_text("frequency=%u kHz\n", config.ds_config);
+		add_text("hop_pattern=%u\n", config.fh_config.hop_pattern);
+		add_text("hop_set=%u\n", config.fh_config.hop_set);
+		add_text("dwell_time=%u msec\n", config.fh_config.dwell_time);
 	}
 
 	res = mp_query(wnd, OID_802_11_TX_POWER_LEVEL,
 		       &tx_power, sizeof(tx_power));
 	if (!res)
-		add_text(p, "tx_power=%u mW\n", tx_power);
+		add_text("tx_power=%u mW\n", tx_power);
 
 	res = mp_query(wnd, OID_GEN_LINK_SPEED, &bit_rate, sizeof(bit_rate));
 	if (!res)
-		add_text(p, "bit_rate=%u kBps\n", (u32)bit_rate / 10);
+		add_text("bit_rate=%u kBps\n", (u32)bit_rate / 10);
 
 	res = mp_query(wnd, OID_802_11_RTS_THRESHOLD,
 		       &rts_threshold, sizeof(rts_threshold));
 	if (!res)
-		add_text(p, "rts_threshold=%u bytes\n", rts_threshold);
+		add_text("rts_threshold=%u bytes\n", rts_threshold);
 
 	res = mp_query(wnd, OID_802_11_FRAGMENTATION_THRESHOLD,
 		       &frag_threshold, sizeof(frag_threshold));
 	if (!res)
-		add_text(p, "frag_threshold=%u bytes\n", frag_threshold);
+		add_text("frag_threshold=%u bytes\n", frag_threshold);
 
 	res = mp_query_int(wnd, OID_802_11_POWER_MODE, &power_mode);
 	if (!res)
-		add_text(p, "power_mode=%s\n",
+		add_text("power_mode=%s\n",
 			 (power_mode == NDIS_POWER_OFF) ? "always_on" :
 			 (power_mode == NDIS_POWER_MAX) ? "max_savings" :
 							  "min_savings");
@@ -221,19 +210,19 @@ static int procfs_read_ndis_hw(char *page, char **start, off_t off,
 	res = mp_query(wnd, OID_802_11_NUMBER_OF_ANTENNAS,
 		       &antenna, sizeof(antenna));
 	if (!res)
-		add_text(p, "num_antennas=%u\n", antenna);
+		add_text("num_antennas=%u\n", antenna);
 
 	res = mp_query(wnd, OID_802_11_TX_ANTENNA_SELECTED,
 		       &antenna, sizeof(antenna));
 	if (!res)
-		add_text(p, "tx_antenna=%u\n", antenna);
+		add_text("tx_antenna=%u\n", antenna);
 
 	res = mp_query(wnd, OID_802_11_RX_ANTENNA_SELECTED,
 		       &antenna, sizeof(antenna));
 	if (!res)
-		add_text(p, "rx_antenna=%u\n", antenna);
+		add_text("rx_antenna=%u\n", antenna);
 
-	add_text(p, "encryption_modes=%s%s%s%s%s%s%s\n",
+	add_text("encryption_modes=%s%s%s%s%s%s%s\n",
 		 test_bit(Ndis802_11Encryption1Enabled, &wnd->capa.encr) ?
 		 "WEP" : "none",
 		 test_bit(Ndis802_11Encryption2Enabled, &wnd->capa.encr) ?
@@ -254,21 +243,16 @@ static int procfs_read_ndis_hw(char *page, char **start, off_t off,
 		if (packet_filter != wnd->packet_filter)
 			WARNING("wrong packet_filter? 0x%08x, 0x%08x\n",
 				packet_filter, wnd->packet_filter);
-		add_text(p, "packet_filter: 0x%08x\n", packet_filter);
-	}
-	if (p - page > count) {
-		WARNING("wrote %td bytes (limit is %u)",
-			p - page, count);
-		*eof = 1;
+		add_text("packet_filter: 0x%08x\n", packet_filter);
 	}
 
-	return p - page;
+	return len;
 }
 
 static int procfs_read_ndis_settings(char *page, char **start, off_t off,
 				     int count, int *eof, void *data)
 {
-	char *p = page;
+	int len = 0;
 	struct ndis_device *wnd = (struct ndis_device *)data;
 	struct wrap_device_setting *setting;
 
@@ -277,18 +261,18 @@ static int procfs_read_ndis_settings(char *page, char **start, off_t off,
 		return 0;
 	}
 
-	add_text(p, "hangcheck_interval=%d\n", (hangcheck_interval == 0) ?
+	add_text("hangcheck_interval=%d\n", (hangcheck_interval == 0) ?
 		 (wnd->hangcheck_interval / HZ) : -1);
 
 	list_for_each_entry(setting, &wnd->wd->settings, list) {
-		add_text(p, "%s=%s\n", setting->name, setting->value);
+		add_text("%s=%s\n", setting->name, setting->value);
 	}
 
 	list_for_each_entry(setting, &wnd->wd->driver->settings, list) {
-		add_text(p, "%s=%s\n", setting->name, setting->value);
+		add_text("%s=%s\n", setting->name, setting->value);
 	}
 
-	return p - page;
+	return len;
 }
 
 static int procfs_write_ndis_settings(struct file *file, const char __user *buf,
@@ -485,7 +469,7 @@ void wrap_procfs_remove_ndis_device(struct ndis_device *wnd)
 static int procfs_read_debug(char *page, char **start, off_t off,
 			     int count, int *eof, void *data)
 {
-	char *p = page;
+	int len = 0;
 #if ALLOC_DEBUG
 	enum alloc_type type;
 #endif
@@ -494,13 +478,13 @@ static int procfs_read_debug(char *page, char **start, off_t off,
 		*eof = 1;
 		return 0;
 	}
-	add_text(p, "%d\n", debug);
+	add_text("%d\n", debug);
 #if ALLOC_DEBUG
 	for (type = 0; type < ALLOC_TYPE_MAX; type++)
-		add_text(p, "total size of allocations in %s: %d\n",
+		add_text("total size of allocations in %s: %d\n",
 			 alloc_type_name[type], alloc_size(type));
 #endif
-	return p - page;
+	return len;
 }
 
 static int procfs_write_debug(struct file *file, const char __user *buf,
